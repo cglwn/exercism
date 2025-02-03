ec6007f9f1e933bbcf490dd922248749c3a4da43
James Cagalawan <james.cagalawan@gmail.com>
2025-02-03 18:22:13 -0500

    Simplify `Port#get_terminal` by using `Symbol#starts_with?` and splat

--- ruby/port-palermo/port_palermo.rb
+++ ruby/port-palermo/port_palermo.rb
@@ -1,18 +1,13 @@
 module Port
   IDENTIFIER = :PALE
+  PORT_A_CARGO = %w(OIL GAS)
 
   def self.get_identifier(city)
     city.slice(0,4).upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    case ship_identifier.to_s.slice(0, 3)
-    when 'OIL'
-      :A
-    when 'GAS'
-      :A
-    else
-      :B
-    end
+    return :A if ship_identifier.start_with?(*PORT_A_CARGO)
+    :B
   end
 end
