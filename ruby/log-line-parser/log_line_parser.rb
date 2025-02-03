313988f3790d6e4d20cceec0abadba5c667cfad7
James Cagalawan <james.cagalawan@gmail.com>
2025-02-03 12:46:06 -0500

    Do `ruby/log-line-parser`

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,17 +1,30 @@
 class LogLineParser
+  ERROR_PREFIX = '[ERROR]: '
+  WARNING_PREFIX = '[WARNING]: '
+  INFO_PREFIX = '[INFO]: '
+
   def initialize(line)
     @line = line
   end
 
   def message
-    raise 'Please implement the LogLineParser#message method'
+    m = @line.gsub(ERROR_PREFIX, '')
+    m = m.gsub(WARNING_PREFIX, '')
+    m = m.gsub(INFO_PREFIX, '')
+    m.strip
   end
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    if @line.start_with?(ERROR_PREFIX)
+      'error'
+    elsif @line.start_with?(WARNING_PREFIX)
+      'warning'
+    elsif @line.start_with?(INFO_PREFIX)
+      'info'
+    end
   end
 
   def reformat
-    raise 'Please implement the LogLineParser#reformat method'
+    "#{message} (#{log_level})"
   end
 end
