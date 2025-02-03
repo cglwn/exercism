1aa0c296644dfb6546f4fa4badc2dbc189a9837f
James Cagalawan <james.cagalawan@gmail.com>
2025-02-03 18:24:56 -0500

    Simplify `Port` methods by using range instead of `slice`

--- ruby/port-palermo/port_palermo.rb
+++ ruby/port-palermo/port_palermo.rb
@@ -2,11 +2,11 @@ module Port
   IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    city.slice(0,4).upcase.to_sym
+    city[0..3].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    case ship_identifier.to_s.slice(0, 3)
+    case ship_identifier.to_s[0..2]
     when 'OIL'
       :A
     when 'GAS'
