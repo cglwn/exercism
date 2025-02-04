822b81dfaa92e21bb8ac583dc64b1e7578c94c0a
James Cagalawan <james.cagalawan@gmail.com>
2025-02-03 20:38:56 -0500

    Do `ruby/chess-game`

--- ruby/chess-game/chess_game.rb
+++ ruby/chess-game/chess_game.rb
@@ -1,16 +1,20 @@
 module Chess
-  # TODO: define the 'RANKS' constant
-  # TODO: define the 'FILES' constant
+  FILES = 'A'..'H'
+  RANKS = 1..8
 
   def self.valid_square?(rank, file)
-    raise "Please implement the Chess.valid_square? method"
+    RANKS.include?(rank) && FILES.include?(file)
   end
 
   def self.nick_name(first_name, last_name)
-    raise "Please implement the Chess.nick_name method"
+    "#{first_name[..1].upcase}#{last_name[-2..].upcase}"
   end
 
   def self.move_message(first_name, last_name, square)
-    raise "Please implement the Chess.move_message method"
+    file, rank = square[0], Integer(square[1])
+    nick_name = nick_name(first_name, last_name)
+    valid_square?(rank, file) ? 
+      "#{nick_name} moved to #{square}" :
+      "#{nick_name} attempted to move to #{square}, but that is not a valid square"
   end
 end
