41a247b3f1a45720e27a26a6ab6cc292a010d526
James Cagalawan <james.cagalawan@gmail.com>
2025-02-05 00:51:51 -0800

    Implement `Blackjack#parse_card` and `Blackjack#card_range`

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,10 +1,30 @@
 module Blackjack
   def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+    case card
+    when 'ace' then 11
+    when 'two' then 2
+    when 'three' then 3
+    when 'four' then 4
+    when 'five' then 5
+    when 'six' then 6
+    when 'seven' then 7
+    when 'eight' then 8
+    when 'nine' then 9
+    when 'ten' then 10
+    when 'jack' then 10
+    when 'queen' then 10
+    when 'king' then 10
+    else 0
+    end
   end
 
   def self.card_range(card1, card2)
-    raise "Please implement the Blackjack.card_range method"
+    case parse_card(card1) + parse_card(card2)
+    when 4..11 then 'low'
+    when 12..16 then 'mid'
+    when 17..20 then 'high'
+    else 'blackjack'
+    end
   end
 
   def self.first_turn(card1, card2, dealer_card)
