97aecc657b1812bb0138c8aead3f3bf6f00e433d
James Cagalawan <james.cagalawan@gmail.com>
2025-03-15 14:20:27 -0400

    Use `private_constant` to make constants public and easy-to-find

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,4 +1,30 @@
 module Blackjack
+  STAND = 'S'
+  private_constant :STAND
+  HIT = 'H'
+  private_constant :HIT
+  SPLIT = 'P'
+  private_constant :SPLIT
+  AUTOMATICALLY_WIN = 'W'
+  private_constant :AUTOMATICALLY_WIN
+
+  CARD_VALUES = {
+    'ace' => 11,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9,
+    'ten' => 10,
+    'jack' => 10,
+    'queen' => 10,
+    'king' => 10,
+  }
+  private_constant :CARD_VALUES
+
   class << self
     def parse_card(card)
       CARD_VALUES.fetch(card, 0)
@@ -40,25 +66,4 @@ module Blackjack
       end
     end
   end
-  private
-    STAND = 'S'
-    HIT = 'H'
-    SPLIT = 'P'
-    AUTOMATICALLY_WIN = 'W'
-
-    CARD_VALUES = {
-      'ace' => 11,
-      'two' => 2,
-      'three' => 3,
-      'four' => 4,
-      'five' => 5,
-      'six' => 6,
-      'seven' => 7,
-      'eight' => 8,
-      'nine' => 9,
-      'ten' => 10,
-      'jack' => 10,
-      'queen' => 10,
-      'king' => 10,
-    }
 end
