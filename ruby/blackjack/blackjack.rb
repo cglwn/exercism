b9f21cc3f4158924f711f76d2f863e0f1f4b5ee6
James Cagalawan <james.cagalawan@gmail.com>
2025-03-15 13:25:12 -0400

    Cleanup by using `Hash` over `case`, using `class << self` for class methods, and hiding private constants

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,61 +1,64 @@
 module Blackjack
-  STAND = 'S'
-  HIT = 'H'
-  SPLIT = 'P'
-  AUTOMATICALLY_WIN = 'W'
-
-  def self.parse_card(card)
-    case card
-    when 'ace' then 11
-    when 'two' then 2
-    when 'three' then 3
-    when 'four' then 4
-    when 'five' then 5
-    when 'six' then 6
-    when 'seven' then 7
-    when 'eight' then 8
-    when 'nine' then 9
-    when 'ten' then 10
-    when 'jack' then 10
-    when 'queen' then 10
-    when 'king' then 10
-    else 0
-    end
-  end
-
-  def self.card_range(card1, card2)
-    case parse_card(card1) + parse_card(card2)
-    when 4..11 then 'low'
-    when 12..16 then 'mid'
-    when 17..20 then 'high'
-    else 'blackjack'
-    end
-  end
-
-  def self.first_turn(card1, card2, dealer_card)
-    if card1 == 'ace' && card2 == 'ace'
-      return SPLIT
+  class << self
+    def parse_card(card)
+      CARD_VALUES.fetch(card, 0)
     end
 
-    case card_range(card1, card2)
-    when 'blackjack'
-      case dealer_card
-      when 'ace', 'ten', 'king', 'queen', 'jack'
-        STAND
-      else
-        AUTOMATICALLY_WIN
+    def card_range(card1, card2)
+      case parse_card(card1) + parse_card(card2)
+      when 4..11 then 'low'
+      when 12..16 then 'mid'
+      when 17..20 then 'high'
+      else 'blackjack'
       end
-    when 'high'
-      STAND
-    when 'mid'
-      case parse_card(dealer_card)
-      when (7..11)
+    end
+
+    def first_turn(card1, card2, dealer_card)
+      if card1 == 'ace' && card2 == 'ace'
+        return SPLIT
+      end
+
+      case card_range(card1, card2)
+      when 'blackjack'
+        case dealer_card
+        when 'ace', 'ten', 'king', 'queen', 'jack'
+          STAND
+        else
+          AUTOMATICALLY_WIN
+        end
+      when 'high'
+        STAND
+      when 'mid'
+        case parse_card(dealer_card)
+        when (7..11)
+          HIT
+        else
+          STAND
+        end
+      else
         HIT
-      else
-        STAND
       end
-    else
-      HIT
     end
   end
+  private
+    STAND = 'S'
+    HIT = 'H'
+    SPLIT = 'P'
+    AUTOMATICALLY_WIN = 'W'
+
+    CARD_VALUES = {
+      'ace' => 11,
+      'two' => 2,
+      'three' => 3,
+      'four' => 4,
+      'five' => 5,
+      'six' => 6,
+      'seven' => 7,
+      'eight' => 8,
+      'nine' => 9,
+      'ten' => 10,
+      'jack' => 10,
+      'queen' => 10,
+      'king' => 10,
+    }
 end
