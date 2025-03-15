8807c92d1337eb23b1f4dbf9bec71e5557ea20e0
James Cagalawan <james.cagalawan@gmail.com>
2025-03-15 16:20:58 -0400

    Finish `ruby/acronym`

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,12 @@
-=begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
-=end
+module Acronym
+  class << self
+    def abbreviate phrase
+      phrase
+        .split(' ')
+        .map { _1.split('-') }
+        .flatten
+        .map { _1[0].upcase }
+        .join('')
+    end
+  end
+end
