b76c273268dd5f0d348adf9a812315b3c01025fb
James Cagalawan <james.cagalawan@gmail.com>
2025-03-16 11:14:37 -0400

    Finish `ruby/dnd-character`

--- ruby/dnd-character/dnd_character.rb
+++ ruby/dnd-character/dnd_character.rb
@@ -1,17 +1,56 @@
-=begin
-Write your code for the 'D&D Character' exercise in this file. Make the tests in
-`dnd_character_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/dnd-character` directory.
-=end
-
 class DndCharacter
-  def self.modifier
-    # Your code here
+  BASE_HITPOINTS = 10
+
+  def self.modifier modifier
+    case modifier
+    when 3, 4
+      modifier - 7
+    when 5, 6
+      modifier - 8
+    when 7, 8
+      modifier - 9
+    when 9, 10
+      modifier - 10
+    when 11, 12
+      modifier - 11
+    when 13, 14
+      modifier - 12
+    when 15, 16
+      modifier - 13
+    when 17, 18
+      modifier - 14
+    end
   end
 
+  attr_reader :strength
+  attr_reader :dexterity
+  attr_reader :constitution
+  attr_reader :intelligence
+  attr_reader :wisdom
+  attr_reader :charisma
+  attr_reader :hitpoints
+  
   def initialize
-    # Your code here
+    prng = Random.new
+
+    rolls = [prng.rand(1..6), prng.rand(1..6), prng.rand(1..6), prng.rand(1..6)]
+    @strength = rolls.max(3).sum
+
+    rolls = [prng.rand(1..6), prng.rand(1..6), prng.rand(1..6), prng.rand(1..6)]
+    @dexterity = rolls.max(3).sum
+
+    rolls = [prng.rand(1..6), prng.rand(1..6), prng.rand(1..6), prng.rand(1..6)]
+    @constitution = rolls.max(3).sum
+    
+    rolls = [prng.rand(1..6), prng.rand(1..6), prng.rand(1..6), prng.rand(1..6)]
+    @intelligence = rolls.max(3).sum
+    
+    rolls = [prng.rand(1..6), prng.rand(1..6), prng.rand(1..6), prng.rand(1..6)]
+    @wisdom = rolls.max(3).sum
+
+    rolls = [prng.rand(1..6), prng.rand(1..6), prng.rand(1..6), prng.rand(1..6)]
+    @charisma = rolls.max(3).sum
+
+    @hitpoints = BASE_HITPOINTS + DndCharacter.modifier(@constitution)
   end
 end
