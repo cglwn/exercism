bebeb55089e81cec4d133f36d206065bba0fe2ab
James Cagalawan <james.cagalawan@gmail.com>
2025-11-17 15:53:18 -0800

    Finish `go/weather-forecast`

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,15 @@
+// Package weather provides weather forecasting functionality.
 package weather
 
 var (
+	// CurrentCondition holds the current weather condition.
 	CurrentCondition string
+
+	// CurrentLocation holds the location to be forecasted.
 	CurrentLocation  string
 )
 
+// Forecast provides a weather forecast for a city's weather conditions.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
