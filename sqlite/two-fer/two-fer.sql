fde8bf4f74574829bbc42c7f5ea16456e16ac43f
James Cagalawan <james.cagalawan@gmail.com>
2025-11-27 12:26:49 -0500

    Finish `sqlite/two-fer`

--- sqlite/two-fer/two-fer.sql
+++ sqlite/two-fer/two-fer.sql
@@ -1,2 +1,11 @@
 -- Schema: CREATE TABLE "twofer" ("input" TEXT, "response" TEXT);
 -- Task: update the twofer table and set the response based on the input.
+UPDATE
+  twofer
+SET
+  response = 'One for ' ||
+  CASE
+    WHEN INPUT = '' THEN 'you'
+    ELSE INPUT
+END
+  || ', one for me.' ;
